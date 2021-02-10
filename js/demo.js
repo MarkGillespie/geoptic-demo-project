a4d8a54d243870c69a3250c9d24fb806fb7cb624
Mark Gillespie <mark.gillespie81@gmail.com>
2021-02-10 13:53:25 -0500

    Add gauss curvature, laplacian eigenvector

--- js/demo.js
+++ js/demo.js
@@ -1,81 +1,78 @@
 import * as THREE from "https://unpkg.com/three@0.125.1/build/three.module.js";
 import { Geoptic } from "./geoptic.js/geoptic.js";
 
-function vertexCoordinates(mesh) {
-  let positions = new Float32Array(V * 3);
-  for (let v of mesh.vertices) {
-    let i = v.index;
-
-    let position = geometry.positions[v];
-    positions[3 * i + 0] = position.x;
-    positions[3 * i + 1] = position.y;
-    positions[3 * i + 2] = position.z;
-  }
-
-  return {
-    positions: positions,
-    get: function (i) {
-      return new THREE.Vector3(
-        this.positions[3 * i + 0],
-        this.positions[3 * i + 1],
-        this.positions[3 * i + 2]
-      );
-    },
-  };
-}
-
-// TODO: support non-triangle meshes
-function faceIndices(mesh) {
-  let F = mesh.faces.length;
-  let indices = new Uint32Array(F * 3);
-  for (let f of mesh.faces) {
-    let i = 0;
-    for (let v of f.adjacentVertices()) {
-      indices[3 * f.index + i++] = v.index;
-    }
-  }
-  return {
-    positions: positions,
-    get: function (i) {
-      return new THREE.Vector3(
-        this.positions[3 * i + 0],
-        this.positions[3 * i + 1],
-        this.positions[3 * i + 2]
-      );
-    },
-  };
-}
+let mesh = undefined;
+let geo = undefined;
+let gpMesh = undefined;
 
 // create geoptic manager
 let geoptic = new Geoptic();
 
-function doWork(text) {}
+function initMesh(meshFile) {
+  let soup = MeshIO.readOBJ(meshFile);
 
-geoptic.userCallback = () => {};
-
-// Once the wasm has loaded, we can start our app
-geoptic.message("webassembly loaded");
-
-// Initialize geoptic
-geoptic.init();
-
-let bunnySoup = MeshIO.readOBJ(bunny);
-let bunnyMesh = new Mesh();
-let bunnyGeo;
-if (bunnyMesh.build(bunnySoup)) {
-  // console.log(bunnyMesh);
-  // bunnyGeo = new Geometry(bunnyMesh, bunnySoup["v"]);
-  let faces = bunnySoup.f;
+  // define size() and get() for geoptic
+  let faces = soup.f;
   faces.size = function () {
     return faces.length / 3;
   };
   faces.get = function (i) {
     return [faces[3 * i], faces[3 * i + 1], faces[3 * i + 2]];
   };
-  geoptic.registerSurfaceMesh("bunny", bunnySoup.v, faces);
-} else {
-  geoptic.warning("unable to build halfedge mesh");
+  gpMesh = geoptic.registerSurfaceMesh("bunny", soup.v, faces);
+
+  mesh = new Mesh();
+  mesh.build(soup);
+  geo = new Geometry(mesh, soup.v);
 }
+
+geoptic.commandGuiFields["K"] = function () {
+  let curvatures = [];
+  for (let v of mesh.vertices) {
+    let iV = v.index;
+    curvatures[iV] = geo.scalarGaussCurvature(v);
+  }
+  let q = gpMesh.addVertexScalarQuantity("K", curvatures);
+  q.setEnabled(true);
+  q.dataMin = -Math.PI / 16;
+  q.dataMax = Math.PI / 16;
+};
+geoptic.commandGui
+  .add(geoptic.commandGuiFields, "K")
+  .name("Compute Gauss Curvature");
+
+geoptic.commandGuiFields["L"] = function () {
+  let vertexIndices = indexElements(geo.mesh.vertices);
+  let L = geo.laplaceMatrix(vertexIndices);
+  let M = geo.massMatrix(vertexIndices);
+
+  let N = L.nRows();
+  let llt = L.chol();
+  let ones = DenseMatrix.ones(N, 1);
+  let x = DenseMatrix.random(N, 1);
+  for (let i = 0; i < 50; i++) {
+    x = llt.solvePositiveDefinite(M.timesDense(x));
+    x.scaleBy(1 / x.norm(2));
+  }
+  let eVec = [];
+  for (let v of geo.mesh.vertices) {
+    let iV = vertexIndices[v];
+    eVec[iV] = x.get(iV, 0);
+  }
+
+  let q = gpMesh.addVertexScalarQuantity("L eigenvector 0", eVec);
+  q.setEnabled(true);
+};
+geoptic.commandGui
+  .add(geoptic.commandGuiFields, "L")
+  .name("Laplacian Eigenvector");
+
+geoptic.userCallback = () => {};
+
+// Initialize geoptic
+geoptic.init();
+
+initMesh(bunny);
 
 geoptic.doneLoading();
 
